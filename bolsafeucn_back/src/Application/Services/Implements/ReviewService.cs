de1ab108d45d31d5d15a9473860b4827f994ab56
Matías Soto Carvajal <matias.soto01@alumnos.ucn.cl>
2025-11-26 14:11:37 -0300

    fix(review): update publication retrieval to use PublicationId instead of review.Id

--- bolsafeucn_back/src/Application/Services/Implements/ReviewService.cs
+++ bolsafeucn_back/src/Application/Services/Implements/ReviewService.cs
@@ -69,7 +69,7 @@ namespace bolsafeucn_back.src.Application.Services.Implements
             var result = new List<PublicationAndReviewInfoDTO>();
             foreach (var review in reviews)
             {
-                var publications = await _repository.GetPublicationInformationAsync(review.Id);
+                var publications = await _repository.GetPublicationInformationAsync(review.PublicationId);
                 if (publications != null)
                 {
                     foreach (var publication in publications)
@@ -109,7 +109,7 @@ namespace bolsafeucn_back.src.Application.Services.Implements
             var result = new List<PublicationAndReviewInfoDTO>();
             foreach (var review in reviews)
             {
-                var publications = await _repository.GetPublicationInformationAsync(review.Id);
+                var publications = await _repository.GetPublicationInformationAsync(review.PublicationId);
                 if (publications != null)
                 {
                     foreach (var publication in publications)
